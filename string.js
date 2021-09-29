bea53f6ee1ad8fe9b9d05c2dc9331a820b3e6109
*ladeed <muhammed.ladeed123@gmail.com>
2021-09-29 09:25:02 +0530

    dsa2

--- string.js
+++ string.js
@@ -1,27 +1,27 @@
-// // //string dsa qns
+//string dsa qns
 
-// // 1.print all the subsequence of a string
-// // let ar=[]
-// // function subseq(init,fin){
+1.print all the subsequence of a string
+let ar=[]
+function subseq(init,fin){
 
-// // if(init.length===0){
-// //     ar.push(fin)
-// //     return
-// // }
+if(init.length===0){
+    ar.push(fin)
+    return
+}
 
-// // let newfin=fin
-// // newfin+=init.charAt(0)
-// // let newInit=init.substring(1,init.length)
-// // subseq(newInit,newfin)
-// // subseq(newInit,fin)
+let newfin=fin
+newfin+=init.charAt(0)
+let newInit=init.substring(1,init.length)
+subseq(newInit,newfin)
+subseq(newInit,fin)
 
-// // }
+}
 
-// // subseq("abc","")
+subseq("abc","")
 
-// // console.log(ar);
+console.log(ar);
 
-// //2.find the permuation of the string "abc"
+2.find the permuation of the string "abc"
 let str="abc"
 let final=""
 let ar=[]
