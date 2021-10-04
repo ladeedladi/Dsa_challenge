ae850cabc4497f92890a2b80adcd99d9cb7b55f0
*ladeed <muhammed.ladeed123@gmail.com>
2021-10-04 09:38:57 +0530

    dp-3 memo added

--- dp.js
+++ dp.js
@@ -1,50 +1,55 @@
-//1.fibonocci 
-let cache={}
-const fib=(n)=>{
-    if(n in cache)return cache[n]
-    if(n<2){
-        return n
-    }
+// //1.fibonocci 
+// let cache={}
+// const fib=(n)=>{
+//     if(n in cache)return cache[n]
+//     if(n<2){
+//         return n
+//     }
     
-   cache[n]=fib(n-1)+fib(n-2)
-   return cache[n]
-}
+//    cache[n]=fib(n-1)+fib(n-2)
+//    return cache[n]
+// }
 
-console.log(fib(6));
-
-
-
-2.grid traveler
-
-const gridTraveler=(m,n,memo={})=>{
-    let key=m+','+n
-    if(key in memo)return memo[key]
-    if(m===1 && n===1)return 1
-    if(m===0 || n===0)return 0
-
-    memo[key]= gridTraveler(m-1,n,memo)+gridTraveler(m,n-1,memo)
-    return memo[key]
-}
+// console.log(fib(6));
 
 
-console.log(gridTraveler(18,18));
 
-3.cansum
+// 2.grid traveler
 
-const cansum=(targetSum,numbers)=>{
+// const gridTraveler=(m,n,memo={})=>{
+//     let key=m+','+n
+//     if(key in memo)return memo[key]
+//     if(m===1 && n===1)return 1
+//     if(m===0 || n===0)return 0
+
+//     memo[key]= gridTraveler(m-1,n,memo)+gridTraveler(m,n-1,memo)
+//     return memo[key]
+// }
+
+
+// console.log(gridTraveler(18,18));
+
+// 3.cansum
+
+const cansum=(targetSum,numbers,memo={})=>{
     //base condition
 if(targetSum===0)return true
 // if it is negetive
 if(targetSum<0)return false
+
+if(targetSum in memo)return memo[targetSum]
 //to iterate thrrough arrays
 for(let num of numbers){
     let remainder=targetSum-num
     //if the basecase return true then it returns true
-    if(cansum(remainder,numbers)===true){
-        return true
+    if(cansum(remainder,numbers,memo)===true){
+      memo[targetSum]=true
+      return memo[targetSum]
     }
 }
-return false
+memo[targetSum]=false
+return memo[targetSum]
 }
 
 console.log(cansum(7,[2,3]));
+console.log(cansum(300,[7,14]));
