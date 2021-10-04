3bd281d04556d06658099dedac6669b3b6ba8863
*ladeed <muhammed.ladeed123@gmail.com>
2021-10-04 09:07:50 +0530

    dp-3

--- dp.js
+++ dp.js
@@ -11,3 +11,40 @@ const fib=(n)=>{
 }
 
 console.log(fib(6));
+
+
+
+2.grid traveler
+
+const gridTraveler=(m,n,memo={})=>{
+    let key=m+','+n
+    if(key in memo)return memo[key]
+    if(m===1 && n===1)return 1
+    if(m===0 || n===0)return 0
+
+    memo[key]= gridTraveler(m-1,n,memo)+gridTraveler(m,n-1,memo)
+    return memo[key]
+}
+
+
+console.log(gridTraveler(18,18));
+
+3.cansum
+
+const cansum=(targetSum,numbers)=>{
+    //base condition
+if(targetSum===0)return true
+// if it is negetive
+if(targetSum<0)return false
+//to iterate thrrough arrays
+for(let num of numbers){
+    let remainder=targetSum-num
+    //if the basecase return true then it returns true
+    if(cansum(remainder,numbers)===true){
+        return true
+    }
+}
+return false
+}
+
+console.log(cansum(7,[2,3]));
